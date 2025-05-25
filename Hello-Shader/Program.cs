25954b9d732d091088d1e133c76feb88547cfb69
SudonNoodle <23106933+SudonNoodle@users.noreply.github.com>
2025-05-25 01:19:45 -0400

    Tutorial Example Code

--- Hello-Shader/Program.cs
+++ Hello-Shader/Program.cs
@@ -1,9 +1,10 @@
-﻿using System.Numerics;
-using System.Text;
+﻿﻿using System.IO;
+using System.Numerics;
 using Veldrid;
 using Veldrid.Sdl2;
-using Veldrid.SPIRV;
 using Veldrid.StartupUtilities;
+using Veldrid.SPIRV;
+using System.Text;
 
 namespace Hello_Shader
 {
@@ -41,7 +42,7 @@ void main()
     fsout_Color = fsin_Color;
 }";
 
-        static void Main()
+        static void Main(string[] args)
         {
             WindowCreateInfo windowCI = new WindowCreateInfo()
             {
@@ -53,14 +54,27 @@ void main()
             };
             Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
 
-            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+            GraphicsDeviceOptions options = new GraphicsDeviceOptions
+            {
+                PreferStandardClipSpaceYDirection = true,
+                PreferDepthRangeZeroToOne = true
+            };
+
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window, options, GraphicsBackend.Vulkan);
 
             CreateResources();
 
             while (window.Exists)
             {
                 window.PumpEvents();
+
+                if (window.Exists)
+                {
+                    Draw();
+                }
             }
+
+            DisposeResources();
         }
 
         private static void CreateResources()
@@ -69,18 +83,22 @@ void main()
 
             VertexPositionColor[] quadVertices =
             {
-                new VertexPositionColor(new Vector2(-0.75f, 0.75f), RgbaFloat.Red),
-                new VertexPositionColor(new Vector2(0.75f, 0.75f), RgbaFloat.Green),
-                new VertexPositionColor(new Vector2(-0.75f, -0.75f), RgbaFloat.Blue),
-                new VertexPositionColor(new Vector2(0.75f, -0.75f), RgbaFloat.Yellow)
+                new VertexPositionColor(new Vector2(-.75f, .75f), RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(.75f, .75f), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-.75f, -.75f), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(.75f, -.75f), RgbaFloat.Yellow)
             };
+            BufferDescription vbDescription = new BufferDescription(
+                4 * VertexPositionColor.SizeInBytes,
+                BufferUsage.VertexBuffer);
+            _vertexBuffer = factory.CreateBuffer(vbDescription);
+            _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, quadVertices);
 
             ushort[] quadIndices = { 0, 1, 2, 3 };
-
-            _vertexBuffer = factory.CreateBuffer(new BufferDescription(4 * VertexPositionColor.SizeInBytes, BufferUsage.VertexBuffer));
-            _indexBuffer = factory.CreateBuffer(new BufferDescription(4 * sizeof(ushort), BufferUsage.IndexBuffer));
-
-            _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, quadVertices);
+            BufferDescription ibDescription = new BufferDescription(
+                4 * sizeof(ushort),
+                BufferUsage.IndexBuffer);
+            _indexBuffer = factory.CreateBuffer(ibDescription);
             _graphicsDevice.UpdateBuffer(_indexBuffer, 0, quadIndices);
 
             VertexLayoutDescription vertexLayout = new VertexLayoutDescription(
@@ -98,44 +116,83 @@ void main()
 
             _shaders = factory.CreateFromSpirv(vertexShaderDesc, fragmentShaderDesc);
 
+            // Create pipeline
             GraphicsPipelineDescription pipelineDescription = new GraphicsPipelineDescription();
             pipelineDescription.BlendState = BlendStateDescription.SingleOverrideBlend;
-
             pipelineDescription.DepthStencilState = new DepthStencilStateDescription(
                 depthTestEnabled: true,
                 depthWriteEnabled: true,
                 comparisonKind: ComparisonKind.LessEqual);
-
             pipelineDescription.RasterizerState = new RasterizerStateDescription(
                 cullMode: FaceCullMode.Back,
                 fillMode: PolygonFillMode.Solid,
                 frontFace: FrontFace.Clockwise,
                 depthClipEnabled: true,
                 scissorTestEnabled: false);
-
             pipelineDescription.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
             pipelineDescription.ResourceLayouts = System.Array.Empty<ResourceLayout>();
-
             pipelineDescription.ShaderSet = new ShaderSetDescription(
                 vertexLayouts: new VertexLayoutDescription[] { vertexLayout },
                 shaders: _shaders);
-
             pipelineDescription.Outputs = _graphicsDevice.SwapchainFramebuffer.OutputDescription;
+
             _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
 
             _commandList = factory.CreateCommandList();
+        }
+
+        private static void Draw()
+        {
+            // Begin() must be called before commands can be issued.
+            _commandList.Begin();
+
+            // We want to render directly to the output window.
+            _commandList.SetFramebuffer(_graphicsDevice.SwapchainFramebuffer);
+            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+
+            // Set all relevant state to draw our quad.
+            _commandList.SetVertexBuffer(0, _vertexBuffer);
+            _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
+            _commandList.SetPipeline(_pipeline);
+            // Issue a Draw command for a single instance with 4 indices.
+            _commandList.DrawIndexed(
+                indexCount: 4,
+                instanceCount: 1,
+                indexStart: 0,
+                vertexOffset: 0,
+                instanceStart: 0);
+
+            // End() must be called before commands can be submitted for execution.
+            _commandList.End();
+            _graphicsDevice.SubmitCommands(_commandList);
+
+            // Once commands have been submitted, the rendered image can be presented to the application window.
+            _graphicsDevice.SwapBuffers();
+        }
+
+        private static void DisposeResources()
+        {
+            _pipeline.Dispose();
+            foreach (Shader shader in _shaders)
+            {
+                shader.Dispose();
+            }
+            _commandList.Dispose();
+            _vertexBuffer.Dispose();
+            _indexBuffer.Dispose();
+            _graphicsDevice.Dispose();
         }
     }
 
     struct VertexPositionColor
     {
-        public Vector2 Position; // This is the position, in normalized device coordinates.
-        public RgbaFloat Color; // This is the color of the vertex.
+        public const uint SizeInBytes = 24;
+        public Vector2 Position;
+        public RgbaFloat Color;
         public VertexPositionColor(Vector2 position, RgbaFloat color)
         {
             Position = position;
             Color = color;
         }
-        public const uint SizeInBytes = 24;
     }
 }
