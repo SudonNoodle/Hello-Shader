47937764885dd92a325b377c27a1e4d75606c3a0
SudonNoodle <23106933+SudonNoodle@users.noreply.github.com>
2025-05-24 23:15:41 -0400

    Window Created

--- Hello-Shader/Program.cs
+++ Hello-Shader/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
 
-Console.WriteLine("Hello, Shader!");
+WindowCreateInfo wci = new WindowCreateInfo()
+{
+    X = 100, Y = 100,
+    WindowWidth = 800, WindowHeight = 600,
+    WindowTitle = "Hello Shader",
+};
+Sdl2Window window = VeldridStartup.CreateWindow(wci);
+GraphicsDeviceOptions gdo = new GraphicsDeviceOptions
+{
+    PreferDepthRangeZeroToOne = true,
+    PreferStandardClipSpaceYDirection = true,
+};
+GraphicsDevice gd = VeldridStartup.CreateGraphicsDevice(window, gdo);
+
+UInt64 ticks = 0;
+while (window.Exists)
+{
+    ticks++;
+    Console.WriteLine($"Ticks: {ticks}");
+    window.PumpEvents();
+    Thread.Sleep(30);
+}
